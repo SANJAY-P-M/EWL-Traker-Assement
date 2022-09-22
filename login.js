379d5b296a9fb4d4ba96c48de72ac7902b283747
SANJAY P M <pmsanjay2003@gmail.com>
2022-09-22 16:05:19 +0530

    designed home homePage

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ fetch('https://randomuser.me/api/?results=10').then((data)=>{
     data.json().then((usableData) => {
        localStorage.setItem( "apiData",JSON.stringify(usableData.results));
         let userData =JSON.parse(localStorage.getItem("apiData"))
-       // console.log(userData[0])
+       //console.log(userData[0])
    })
 })
 function check(){
