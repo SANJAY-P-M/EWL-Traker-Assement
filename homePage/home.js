779c5ba6ac8f1d03fd7079b957b1b85d865fd797
SANJAY P M <pmsanjay2003@gmail.com>
2022-09-22 16:04:56 +0530

    home page design

--- homePage/home.js
+++ homePage/home.js
@@ -1,7 +1,12 @@
 fetch("https://randomuser.me/api/?results=10").then((fetchedpromise) =>{
     fetchedpromise.json().then((jsonpromise) => {
-        localStorage.setItem("userdetails",JSON.stringify(jsonpromise))
-        y = JSON.phaselocalstorage.getItem("userdetails")
+        localStorage.setItem("userdetails",JSON.stringify(jsonpromise.results))
+       let  y = JSON.parse(localStorage.getItem("userdetails"))
+       let data =""
+       y.forEach(element => {
+           data += "<div class="card"><figure><img src=/"${element.picture.medium}/" ><figcaption>Apple</figcaption></figure></div>"
+        });
+        console.log(data)
+        document.getElementsByName("main").innerHTML=data;
     })
 })
-var data= "<div class="card"><figure><img src="apple.jpg" ><figcaption>Apple</figcaption></figure></div>"
