efd54751dd86eb89a7c919341276579a27e5ff1f
SANJAY P M <pmsanjay2003@gmail.com>
2022-09-23 06:13:15 +0530

    Login validation complected
    Yep javascript is more interesting than I my expectation

--- login.js
+++ login.js
@@ -1,10 +1,17 @@
-fetch('https://randomuser.me/api/?results=10').then((data)=>{
-    data.json().then((usableData) => {
-       localStorage.setItem( "apiData",JSON.stringify(usableData.results));
-        let userData =JSON.parse(localStorage.getItem("apiData"))
-       //console.log(userData[0])
-   })
+fetch('https://randomuser.me/api/?results=10').then((fetchedPromise) => {
+    fetchedPromise.json().then((jsonPromise) => {
+        localStorage.setItem("validation-details",JSON.stringify(jsonPromise.results))
+    })
 })
 function check(){
-    
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    JSON.parse(localStorage.getItem("validation-details")).forEach(element => {
+        if(element.login.username = username && element.login.password == password)
+            location.replace("home.html")
+        else
+            document.getElementById("notice").innerHTML = "Doode check your passwordv! <br> Try again :)"
+    });
+    //console.log();
+
 }
