6e586585ec8a6dc0e03516784c27a6371deab04e
SANJAY P M <pmsanjay2003@gmail.com>
2022-09-26 06:54:57 +0530

    Almost got the thing to make a card
    Sudden idea at morning and implemented during the rush to college xD

--- home.js
+++ home.js
@@ -1,13 +1,13 @@
+let data =``;
 fetch("https://randomuser.me/api/?results=10").then((fetchedpromise) =>{
     fetchedpromise.json().then((jsonpromise) => {
         localStorage.setItem("userdetails",JSON.stringify(jsonpromise.results))
        let  y = JSON.parse(localStorage.getItem("userdetails"))
        console.log(y)
-       let data =""
-      // y.forEach(element => {
-           //data += "<div class="card"><figure><img src=/"${element.picture.medium}/" ><figcaption>Apple</figcaption></figure></div>"
-     //   });
-        //console.log(data)
-        document.getElementsByName("main").innerHTML=data;
+       y.forEach(element => {
+           data += `<div class="card"><figure><img src="${element.picture.medium}" ><figcaption></figcaption></figure></div>`
+           document.getElementById("main").innerHTML += data
+           console.log(data)
+        });
     })
 })
